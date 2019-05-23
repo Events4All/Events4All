d78cd79eca5e5ce3bbfc4527a66a6050f49772cd
Andrew DiBacco <S01224226@acad.tri-c.edu>
2019-05-23 10:32:42 -0400

    Corrected connection strings

--- Events4All.DB/Models/IdentityModels.cs
+++ Events4All.DB/Models/IdentityModels.cs
@@ -20,8 +20,7 @@ namespace Events4All.DB.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        public ApplicationDbContext() : base("Tech4AllDB", throwIfV1Schema: false)
         {
         }
 
