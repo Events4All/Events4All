e4a116698986fcc9b8a4d9736be61c2bbdc78b2f
jrhos97@gmail.com <jrhos97@gmail.com>
2019-06-13 15:15:15 -0400

    Added DB migration fix

---
+++ Events4All.DB/Migrations/201906131831155_MigrationFix.cs
@@ -0,0 +1,16 @@
+namespace Events4All.DB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrationFix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
